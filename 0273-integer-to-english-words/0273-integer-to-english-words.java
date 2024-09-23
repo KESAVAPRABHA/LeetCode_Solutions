59bb46ecded034a8219ba46993afb5b99bf68b87
L.Kesava Prabha <125806834+KESAVAPRABHA@users.noreply.github.com>
2024-09-23 13:27:08 +0530

    Time: 12 ms (20.17%), Space: 41.5 MB (97.06%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    private static final String[] LESS_THAN_20 = {
+            "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    private static final String[] TENS = {
+            "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
+    private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
+    public static String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+
+        int i = 0;
+        String words = "";
+
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                words = processSegment(num % 1000) + THOUSANDS[i] + " " + words;
+            }
+            num /= 1000;
+            i++;
+        }
+
+        return words.trim();
+    }
+
+    private static String processSegment(int num) {
+        if (num == 0) {
+            return "";
+        } else if (num < 20) {
+            return LESS_THAN_20[num] + " ";
+        } else if (num < 100) {
+            return TENS[num / 10] + " " + processSegment(num % 10);
+        } else {
+            return LESS_THAN_20[num / 100] + " Hundred " + processSegment(num % 100);
+        }
+    }
+
+    public static void main(String[] args) {
+        int num1 = 123;
+        System.out.println(numberToWords(num1));
+
+        int num2 = 12345;
+        System.out.println(numberToWords(num2));
+
+        int num3 = 1234567;
+        System.out.println(numberToWords(num3));
+    }
+}
