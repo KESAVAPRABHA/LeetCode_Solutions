ca60969cf317704ac437d4c2c00247b53e6a4261
L.Kesava Prabha <125806834+KESAVAPRABHA@users.noreply.github.com>
2024-09-24 10:16:42 +0530

    Time: 16 ms (41.57%), Space: 58.6 MB (23.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,44 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashMap<Integer,Integer>map=new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+//         }
+//         for(int i=0;i<map.size();i++){
+//             if(map.get(nums[i])>=2){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length;
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] == nums[i - 1])
+//                 return true;
+//         }
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(int i=0;i<map.size();i++){
-            if(map.get(nums[i])>=2){
-                return true;
+            int n = nums.length; // Step 1: Initialize variable to store length of nums
+            Set<Integer> set = new HashSet<>(); // Step 2: Create HashSet to store unique elements
+            for(int num : nums){
+                set.add(num); // Step 3: Add elements of nums to the HashSet
+            }
+            int m = set.size(); // Step 4: Get the size of the HashSet
+            if(n==m){    //Step 5: Compare lengths and return result
+                return false; //Return false if there were  no duplicates, true otherwise
+            }
+            return true;   
             }
         }
-        return false;
-    }
-}
