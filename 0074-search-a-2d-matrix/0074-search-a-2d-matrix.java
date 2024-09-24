41172331199fce93b924c9b4191952f62f94f039
L.Kesava Prabha <125806834+KESAVAPRABHA@users.noreply.github.com>
2024-09-24 11:32:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (68.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,12 +1,41 @@
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 if(matrix[i][j]==target){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        // Treat the 2D matrix as a flattened 1D array
+        int left = 0, right = m * n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midElement = matrix[mid / n][mid % n];
+            
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
+        
         return false;
     }
 }
