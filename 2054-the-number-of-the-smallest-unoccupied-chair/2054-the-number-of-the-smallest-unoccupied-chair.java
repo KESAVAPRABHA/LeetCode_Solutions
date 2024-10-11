cd2026554083e81d8ff874ccc98b6fa52b064f86
L.Kesava Prabha <125806834+KESAVAPRABHA@users.noreply.github.com>
2024-10-11 13:04:25 +0530

    Time: 46 ms (77.78%), Space: 48.8 MB (67.84%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
@@ -7,16 +7,16 @@ class Solution {
             pq.offer(i);
         }
         PriorityQueue<int[]>pq1=new PriorityQueue<>((a,b) -> a[0]-b[0]);
-        for(int i=0;i<times.length;i++){
-            while(!pq1.isEmpty() && pq1.peek()[0]<=times[i][0]){
-                pq.offer(pq1.poll()[1]);
+        for (int i = 0; i < times.length; i++) {
+            while (!pq1.isEmpty() && pq1.peek()[0] <= times[i][0]) {
+                pq.offer(pq1.poll()[1]); // Free up chairs as friends leave
             }
-            if(times[i][0]==target){
-                break;
+            if (times[i][0] == target) { // When we reach the target friend's arrival time
+                break; // Stop the loop, since we only care about the target friend
             }
-            pq1.offer(new int[]{ times[i][1], pq.poll()});
-
+            pq1.offer(new int[]{times[i][1], pq.poll()}); // Assign the next available chair to the current friend
         }
+
         return pq.peek();
     }
 }
